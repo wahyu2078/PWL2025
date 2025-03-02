af2494dfb26ac9912a1487f4cf2f88c8b978fc72
wahyu2078 <wahyutptp20@gmail.com>
2025-03-02 22:27:56 +0700

    minggu 2

---
+++ Minggu2/POS/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+
+// Halaman Home
+Route::get('/', [HomeController::class, 'index']);
+
+// Halaman Products dengan Prefix
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+});
+
+// Halaman User dengan Parameter
+Route::get('/user/{id}/name/{name}', [UserController::class, 'show']);
+
+// Halaman Penjualan (POS)
+Route::get('/sales', [SalesController::class, 'index']);
