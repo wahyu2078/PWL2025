99d444b1a296fec8bdd7610c6eb5a63b387e76d4
wahyu2078 <wahyutptp20@gmail.com>
2025-03-23 13:10:20 +0700

    Penerapan Layouting

--- Minggu5/POS_minggu5/resources/views/layouts/breadcrumb.blade.php
+++ Minggu5/POS_minggu5/resources/views/layouts/breadcrumb.blade.php
@@ -1,15 +1,18 @@
 <section class="content-header">
-      <div class="container-fluid">
+    <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>Blank Page</h1>
-          </div>
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Blank Page</li>
-            </ol>
-          </div>
+            <div class="col-sm-6">
+                <h1>{{ $breadcrumb->title }}</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    @foreach($breadcrumb->list as $key => $value)
+                        <li class="breadcrumb-item {{ $key == count($breadcrumb->list) - 1 ? 'active' : '' }}">
+                            {{ $value }}
+                        </li>
+                    @endforeach
+                </ol>
+            </div>
         </div>
-      </div><!-- /.container-fluid -->
-    </section>
+    </div>
+</section>
