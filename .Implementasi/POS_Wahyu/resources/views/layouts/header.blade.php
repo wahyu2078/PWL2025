3b5536042063e2ba9f46ab75d97bf0e8377ba982
wahyu2078 <wahyutptp20@gmail.com>
2025-04-12 05:49:07 +0700

    pembentukan MultiLevel Authorization

--- .Implementasi/POS_Wahyu/resources/views/layouts/header.blade.php
+++ .Implementasi/POS_Wahyu/resources/views/layouts/header.blade.php
@@ -1,6 +1,4 @@
-<!-- Main Header -->
-<nav class="main-header navbar navbar-expand navbar-white navbar-light shadow-sm">
-
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
         <li class="nav-item">
@@ -9,128 +7,70 @@
             </a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Beranda</a>
+            <a href="#" class="nav-link">Home</a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Kontak</a>
+            <a href="#" class="nav-link">Contact</a>
         </li>
     </ul>
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
+        <!-- Info Login -->
+        @auth
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">
+                <i class="fas fa-user"></i>
+                {{ Auth::user()->nama }} ({{ Auth::user()->getRoleName() }})
+            </a>
+        </li>
+        @endauth
 
         <!-- Search -->
         <li class="nav-item">
-            <a class="nav-link" data-widget="navbar-search" href="#" role="button" title="Cari...">
+            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
                 <i class="fas fa-search"></i>
             </a>
             <div class="navbar-search-block">
                 <form class="form-inline">
                     <div class="input-group input-group-sm">
-                        <input class="form-control form-control-navbar" type="search" placeholder="Cari..." aria-label="Search">
+                        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
                         <div class="input-group-append">
-                            <button class="btn btn-navbar" type="submit"><i class="fas fa-search"></i></button>
-                            <button class="btn btn-navbar" type="button" data-widget="navbar-search"><i class="fas fa-times"></i></button>
+                            <button class="btn btn-navbar" type="submit">
+                                <i class="fas fa-search"></i>
+                            </button>
+                            <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                                <i class="fas fa-times"></i>
+                            </button>
                         </div>
                     </div>
                 </form>
             </div>
         </li>
 
-        <!-- Messages -->
-        <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#" title="Pesan">
-                <i class="far fa-comments"></i>
-                <span class="badge badge-danger navbar-badge">3</span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                @foreach ([1, 2, 3] as $i)
-                <a href="#" class="dropdown-item">
-                    <div class="media">
-                        <img src="../../dist/img/user{{ $i }}-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle shadow-sm">
-                        <div class="media-body">
-                            <h3 class="dropdown-item-title mb-1">
-                                User {{ $i }}
-                                <span class="float-right text-sm text-{{ ['danger','muted','warning'][$i - 1] }}">
-                                    <i class="fas fa-star"></i>
-                                </span>
-                            </h3>
-                            <p class="text-sm">Pesan dari user {{ $i }}</p>
-                            <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> {{ $i * 2 }} jam lalu</p>
-                        </div>
-                    </div>
-                </a>
-                @if($i < 3)
-                    <div class="dropdown-divider">
-            </div>
-            @endif
-            @endforeach
-            <a href="#" class="dropdown-item dropdown-footer text-center text-primary">Lihat Semua Pesan</a>
-            </div>
-        </li>
-
-        <!-- Notifications -->
-        <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#" title="Notifikasi">
-                <i class="far fa-bell"></i>
-                <span class="badge badge-warning navbar-badge">15</span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                <span class="dropdown-item dropdown-header">15 Notifikasi</span>
-                <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item">
-                    <i class="fas fa-envelope mr-2"></i> 4 pesan baru
-                    <span class="float-right text-muted text-sm">3 menit</span>
-                </a>
-                <a href="#" class="dropdown-item">
-                    <i class="fas fa-users mr-2"></i> 8 permintaan teman
-                    <span class="float-right text-muted text-sm">12 jam</span>
-                </a>
-                <a href="#" class="dropdown-item">
-                    <i class="fas fa-file mr-2"></i> 3 laporan baru
-                    <span class="float-right text-muted text-sm">2 hari</span>
-                </a>
-                <a href="#" class="dropdown-item dropdown-footer text-center text-primary">Lihat Semua Notifikasi</a>
-            </div>
-        </li>
-
         <!-- Fullscreen -->
         <li class="nav-item">
-            <a class="nav-link" data-widget="fullscreen" href="#" role="button" title="Layar Penuh">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
             </a>
         </li>
 
         <!-- Control Sidebar -->
         <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button" title="Sidebar">
+            <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
                 <i class="fas fa-th-large"></i>
             </a>
         </li>
+
+        <!-- Logout -->
         <li class="nav-item">
-
-
             <a href="{{ url('/logout') }}" class="nav-link"
-
-
-                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-
-
+               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                 <i class="fas fa-sign-out-alt"></i> Logout
-
-
             </a>
-
-
             <form id="logout-form" action="{{ url('/logout') }}" method="GET" style="display: none;">
-
-
                 @csrf
-
-
             </form>
-
-
         </li>
     </ul>
 </nav>
--- .Implementasi/POS_Wahyu/routes/web.php
+++ .Implementasi/POS_Wahyu/routes/web.php
@@ -1,81 +1,89 @@
 <?php
 
-use App\Http\Controllers\BarangController;
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\LevelController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\WelcomeController;
-use App\Http\Controllers\SupplierController;
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\SupplierController;
+use App\Http\Controllers\BarangController;
+
+Route::pattern('id', '[0-9]+');
 
 // Halaman Welcome
 Route::get('/', [WelcomeController::class, 'index']);
-Route::get('login', [AuthController::class, 'login'])->name('login');
-Route::post('login', [AuthController::class, 'postlogin']);
-Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
 
-// User (AJAX )
-Route::prefix('user')->group(function () {
-    Route::get('/', [UserController::class, 'index']);
-    Route::post('/list', [UserController::class, 'list']);
-    Route::get('/create_ajax', [UserController::class, 'create_ajax']);
-    Route::post('/ajax', [UserController::class, 'store_ajax']);
-    Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
-    Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
-    Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);
-    Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
-    Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']);
-});
+// Autentikasi
+Route::get('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'postlogin']);
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth');
 
-// Level (AJAX )
-Route::prefix('level')->group(function () {
-    Route::get('/', [LevelController::class, 'index']);
-    Route::post('/list', [LevelController::class, 'list']);
-    Route::get('/create_ajax', [LevelController::class, 'create_ajax']);
-    Route::post('/ajax', [LevelController::class, 'store_ajax']);
-    Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']);
-    Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']);
-    Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']);
-    Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']);
-    Route::get('/{id}/show_ajax', [LevelController::class, 'show_ajax']);
-});
+// Route yang hanya bisa diakses jika sudah login
+Route::middleware(['auth'])->group(function () {
 
-// Kategori (AJAX )
-Route::prefix('kategori')->group(function () {
-    Route::get('/', [KategoriController::class, 'index']);
-    Route::post('/list', [KategoriController::class, 'list']);
-    Route::get('/create_ajax', [KategoriController::class, 'create_ajax']);
-    Route::post('/ajax', [KategoriController::class, 'store_ajax']);
-    Route::get('/{id}/edit_ajax', [KategoriController::class, 'edit_ajax']);
-    Route::put('/{id}/update_ajax', [KategoriController::class, 'update_ajax']);
-    Route::get('/{id}/delete_ajax', [KategoriController::class, 'confirm_ajax']);
-    Route::delete('/{id}/delete_ajax', [KategoriController::class, 'delete_ajax']);
-    Route::get('/{id}/show_ajax', [KategoriController::class, 'show_ajax']);
-});
+    // ======= User (AJAX Only) =======
+    Route::prefix('user')->group(function () {
+        Route::get('/', [UserController::class, 'index']);
+        Route::post('/list', [UserController::class, 'list']);
+        Route::get('/create_ajax', [UserController::class, 'create_ajax']);
+        Route::post('/ajax', [UserController::class, 'store_ajax']);
+        Route::get('/{id}/edit_ajax', [UserController::class, 'edit_ajax']);
+        Route::put('/{id}/update_ajax', [UserController::class, 'update_ajax']);
+        Route::get('/{id}/delete_ajax', [UserController::class, 'confirm_ajax']);
+        Route::delete('/{id}/delete_ajax', [UserController::class, 'delete_ajax']);
+        Route::get('/{id}/show_ajax', [UserController::class, 'show_ajax']);
+    });
 
-// Barang (AJAX )
-Route::prefix('barang')->group(function () {
-    Route::get('/', [BarangController::class, 'index']);
-    Route::post('/list', [BarangController::class, 'list']);
-    Route::get('/create_ajax', [BarangController::class, 'create_ajax']);
-    Route::post('/ajax', [BarangController::class, 'store_ajax']);
-    Route::get('/{id}/edit_ajax', [BarangController::class, 'edit_ajax']);
-    Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']);
-    Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
-    Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
-    Route::get('/{id}/show_ajax', [BarangController::class, 'show_ajax']);
-});
+    // ======= Level (AJAX Only - ADM) =======
+    Route::middleware(['authorize:ADM'])->prefix('level')->group(function () {
+        Route::get('/', [LevelController::class, 'index']);
+        Route::post('/list', [LevelController::class, 'list']);
+        Route::get('/create_ajax', [LevelController::class, 'create_ajax']);
+        Route::post('/ajax', [LevelController::class, 'store_ajax']);
+        Route::get('/{id}/edit_ajax', [LevelController::class, 'edit_ajax']);
+        Route::put('/{id}/update_ajax', [LevelController::class, 'update_ajax']);
+        Route::get('/{id}/delete_ajax', [LevelController::class, 'confirm_ajax']);
+        Route::delete('/{id}/delete_ajax', [LevelController::class, 'delete_ajax']);
+        Route::get('/{id}/show_ajax', [LevelController::class, 'show_ajax']);
+    });
 
-// Supplier (AJAX)
-Route::prefix('supplier')->group(function () {
-    Route::get('/', [SupplierController::class, 'index']);
-    Route::post('/list', [SupplierController::class, 'list']);
-    Route::get('/create_ajax', [SupplierController::class, 'create_ajax']);
-    Route::post('/ajax', [SupplierController::class, 'store_ajax']);
-    Route::get('/{id}/edit_ajax', [SupplierController::class, 'edit_ajax']);
-    Route::put('/{id}/update_ajax', [SupplierController::class, 'update_ajax']);
-    Route::get('/{id}/delete_ajax', [SupplierController::class, 'confirm_ajax']);
-    Route::delete('/{id}/delete_ajax', [SupplierController::class, 'delete_ajax']);
-    Route::get('/{id}/show_ajax', [SupplierController::class, 'show_ajax']);
+    // ======= Kategori (AJAX Only) =======
+    Route::middleware(['authorize:ADM,MNG'])->prefix('kategori')->group(function () {
+        Route::get('/', [KategoriController::class, 'index']);
+        Route::post('/list', [KategoriController::class, 'list']);
+        Route::get('/create_ajax', [KategoriController::class, 'create_ajax']);
+        Route::post('/ajax', [KategoriController::class, 'store_ajax']);
+        Route::get('/{id}/edit_ajax', [KategoriController::class, 'edit_ajax']);
+        Route::put('/{id}/update_ajax', [KategoriController::class, 'update_ajax']);
+        Route::get('/{id}/delete_ajax', [KategoriController::class, 'confirm_ajax']);
+        Route::delete('/{id}/delete_ajax', [KategoriController::class, 'delete_ajax']);
+        Route::get('/{id}/show_ajax', [KategoriController::class, 'show_ajax']);
+    });    
+
+    // ======= Barang (AJAX Only - ADM & MNG) =======
+    Route::middleware(['authorize:ADM,MNG,STF'])->prefix('barang')->group(function () {
+        Route::get('/', [BarangController::class, 'index']);
+        Route::post('/list', [BarangController::class, 'list']);
+        Route::get('/create_ajax', [BarangController::class, 'create_ajax']);
+        Route::post('/ajax', [BarangController::class, 'store_ajax']);
+        Route::get('/{id}/edit_ajax', [BarangController::class, 'edit_ajax']);
+        Route::put('/{id}/update_ajax', [BarangController::class, 'update_ajax']);
+        Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
+        Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
+        Route::get('/{id}/show_ajax', [BarangController::class, 'show_ajax']);
+    });
+
+    // ======= Supplier (AJAX Only) =======
+    Route::prefix('supplier')->group(function () {
+        Route::get('/', [SupplierController::class, 'index']);
+        Route::post('/list', [SupplierController::class, 'list']);
+        Route::get('/create_ajax', [SupplierController::class, 'create_ajax']);
+        Route::post('/ajax', [SupplierController::class, 'store_ajax']);
+        Route::get('/{id}/edit_ajax', [SupplierController::class, 'edit_ajax']);
+        Route::put('/{id}/update_ajax', [SupplierController::class, 'update_ajax']);
+        Route::get('/{id}/delete_ajax', [SupplierController::class, 'confirm_ajax']);
+        Route::delete('/{id}/delete_ajax', [SupplierController::class, 'delete_ajax']);
+        Route::get('/{id}/show_ajax', [SupplierController::class, 'show_ajax']);
+    });
 });
